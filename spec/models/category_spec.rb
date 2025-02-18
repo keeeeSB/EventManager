c66a5b8e59de3116b0347d05da609e5b1ff3050e
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-19 08:45:34 +0900

    Categoryモデルにバリデーションを追加

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  let(:category) { FactoryBot.create(:category) }
+  
   it "ファクトリーが正常に動く" do
     expect(FactoryBot.build(:category)).to be_valid
+  end
+
+  it "正常な値を入力すると、カテゴリーが作成できる" do
+    ategory = Category.new(
+      name: "スポーツ"
+    )
+    expect(category).to be_valid
+  end
+
+  describe "バリデーションのテスト" do
+    it "カテゴリー名は必須である" do
+      category.name = " "
+      expect(category).to_not be_valid
+    end
   end
 end
