8395546544213212250429280684173330503c92
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-19 09:42:20 +0900

    rubocopによる修正

--- db/migrate/20250219003031_create_events.rb
+++ db/migrate/20250219003031_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[7.2]
   def change
     create_table :events do |t|
       t.string :title, null: false
-      t.text :description, null:false
+      t.text :description, null: false
       t.datetime :start_time, null: false
       t.string :location, null: false
       t.references :user, null: false, foreign_key: true
