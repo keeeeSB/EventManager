07efd65adccc2cb3f622838437a028bdb47a3351
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-24 17:21:13 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
 
   resources :users, only: [ :show ] do
-    resources :events
+    resources :events, only: [ :new, :create, :show, :edit, :update, :destroy ]
   end
+  resources :events, only: [ :index ]
 end
