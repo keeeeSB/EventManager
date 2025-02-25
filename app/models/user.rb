025775068cca44bf8416120f3891f095e3bf2f7c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-26 04:22:04 +0900

    Favoriteモデルのリレーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   mount_uploader :profile_image, ProfileImageUploader
 
   has_many :events, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_events, through: :favorites, source: :event
 
   validates :name, presence: true
   validates :bio, length: { maximum: 100 }
