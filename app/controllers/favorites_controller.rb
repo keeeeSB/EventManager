7295cb0ea9c45d0ad9efb1b20d046b7e70ff760f
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-03-02 21:40:07 +0900

    お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,14 @@
 class FavoritesController < ApplicationController
-  before_action :set_event
-
   def create
-    @favorite = current_user.favorites.create!(event_id: event.id)
+    @favorite = current_user.favorites.create!(event_id: params[:event_id])
     flash[:success] = "お気に入りに追加しました。"
     redirect_to events_path
   end
 
   def destroy
-    @favorite = current_user.favorites.find_by(event_id: event.id)
+    @favorite = current_user.favorites.find_by(event_id: params[:event_id])
     @favorite.destroy!
     flash[:success] = "お気に入りを解除しました。"
     redirect_to events_path, status: :see_other
   end
-
-  private
-
-    def set_event
-      event = Event.find(params[:id])
-    end
 end
