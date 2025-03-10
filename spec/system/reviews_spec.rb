7fef1353b8c33b6fd41569dceaedfdfd9d09beb8
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-03-11 00:11:54 +0900

    レビュー機能のテストを追加

---
+++ spec/system/reviews_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Reviews", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:event) { FactoryBot.create(:event) }
+
+  before do
+    sign_in user
+    visit events_path
+  end
+
+  scenario "ユーザーはイベントに対してレビューができる" do
+    visit user_event_path(user, event)
+    expect {
+      choose "star5"
+      fill_in "コメント", with: "とてもよかったです。"
+      click_button "投稿"
+    }.to change(Review, :count).by(1)
+
+    expect(page).to have_content "レビューを投稿しました。"
+    expect(page).to have_content "とてもよかったです。"
+  end
+end
