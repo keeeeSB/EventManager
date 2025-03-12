10d200976bf314dc9a3f0cd8688ad4c9cf5b38c0
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-03-12 12:23:49 +0900

    レビュー機能の修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,28 +2,32 @@ class ReviewsController < ApplicationController
   before_action :set_event
 
   def create
-    @review = current_user.review.create!(event_id: params[:event_id])
+    @review = current_user.reviews.create!(review_params)
     flash[:success] = "レビューを投稿しました。"
-    redirect_to user_event_path(event.user, event)
+    redirect_to user_event_path(@event.user, @event)
   end
 
   def update
-    @review = current_user.review.find_by(event_id: params[:event_id])
-    @review.update!
+    @review = current_user.reviews.find_by(event_id: @event.id)
+    @review.update!(review_params)
     flash[:success] = "レビューを更新しました。"
-    redirect_to user_event_path(event.user, event)
+    redirect_to user_event_path(@event.user, @event)
   end
 
   def destroy
-    @review = current_user.review.find_by(event_id: params[:event_id])
+    @review = current_user.reviews.find_by(event_id: @event.id)
     @review.destroy!
     flash[:success] = "レビューを削除しました。"
-    redirect_to user_event_path(event.user, event), status: :see_other
+    redirect_to user_event_path(@event.user, @event), status: :see_other
   end
 
   private
 
+    def review_params
+      params.require(:review).permit(:body, :rating).merge(event_id: params[:event_id])
+    end
+
     def set_event
-      event = Event.find(params[:id])
+      @event = Event.find(params[:event_id])
     end
 end
